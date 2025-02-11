8efda8da993de3e1c62f1c104a204d20671eb9b1
Oleg <Oleg-BLR@yandex.ru>
2025-02-11 22:00:22 +0300

    3 buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Custom Buttons
+const Button_01 = ({ children }) => {
+  return <button className="button_01">{children}</button>;
+};
+
+const Button_02 = ({ children }) => {
+  return <button className="button_02">{children}</button>;
+};
+
+const Button_03 = ({ children }) => {
+  return <button className="button_03">{children}</button>;
+};
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
-    <Button variant="primary">Button</Button>
+      <h1>Hello me</h1>
+      <Button_01>Button_01</Button_01>
+      <Button_02>Button_02</Button_02>
+      <Button_03>Button_03</Button_03>
     </>
   );
 }
